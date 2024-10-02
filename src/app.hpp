4f596ceda5bbc3f3a3df39859936e644601dab98
Ethan Mizer <68983654+SrMeissel@users.noreply.github.com>
2024-10-02 01:44:09 -0400

    further setup project
    added gtk4 as dep, setup ros to work with clangd in nvim :)

--- src/app.hpp
+++ src/app.hpp
@@ -1,6 +1,15 @@
 #include <cstdio>
 
-class App {
+#include <gtk/gtk.h>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+class Surface : public rclcpp::Node {
 public:
-  void run(int argc, char **argv);
+  Surface() : Node("controlSurface") {
+    // setup;
+  }
+
+private:
 };
