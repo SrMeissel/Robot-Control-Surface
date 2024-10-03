f92b46f9c30fb83261e28b22345fc33d2c5b997d
Ethan Mizer <68983654+SrMeissel@users.noreply.github.com>
2024-10-03 19:14:28 -0400

    created GTK hello world within node
    modified Cmake to recognize GObject functions with
    target_include_libraries BEFORE ament_target_dependencies.

--- src/app.cpp
+++ src/app.cpp
@@ -1,10 +1,21 @@
 #include "app.hpp"
 
-Surface::Surface() : Node("controlSurface") {}
+static void activate(GtkApplication *app, gpointer user_data) {
+  (void)user_data;
+  GtkWidget *window = gtk_application_window_new(app);
+  gtk_window_set_title(GTK_WINDOW(window), "General Kenobi!");
+  gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
 
-Surface::~Surface() {
-  // g_object_unref(app);
+  gtk_window_present(GTK_WINDOW(window));
 }
+
+Surface::Surface() : Node("controlSurface") {
+  app = gtk_application_new("ros.controlSurface", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  g_application_run(G_APPLICATION(app), 0, NULL);
+}
+
+Surface::~Surface() { g_object_unref(app); }
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
